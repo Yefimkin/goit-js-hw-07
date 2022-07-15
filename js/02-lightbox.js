184d677b01aab2e81a468c5447bf420990969470
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-15 16:17:00 +0300

    fix bugs, made 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+function galleryRender(arrayOfImages) {
+    const arrayOfImg = [];
+    galleryItems.map(({ preview, original, description }) => {
+        arrayOfImg.push(`<a class="gallery__item" href="${original}">
+                            <img class="gallery__image" 
+                            src="${preview}" 
+                            alt="${description}" />
+                        </a>`);
+    });
+    galleryRef.insertAdjacentHTML('beforeend', arrayOfImg.join(''));
+}
+galleryRender(galleryItems);
+
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.9,
+    loop: true,
+});
