6aa18730c2ce2b19783b488b702a438d21717bd8
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-16 20:26:17 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,9 +8,9 @@ const galleryRef = document.querySelector('.gallery');
 // 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 
 function galleryRender(arrayOfImages) {
-    const arrayOfImg = [];
-    galleryItems.map(({ preview, original, description }) => {
-        arrayOfImg.push(`<div class="gallery__item">
+  const arrayOfImg = [];
+  galleryItems.map(({ preview, original, description }) => {
+    arrayOfImg.push(`<div class="gallery__item">
                             <a class="gallery__link" href="large-image.jpg">
                                 <img
                                     class="gallery__image"
@@ -20,29 +20,32 @@ function galleryRender(arrayOfImages) {
                                 />
                             </a>
                         </div>`);
-    });
-    galleryRef.insertAdjacentHTML('beforeend', arrayOfImg.join(''));
+  });
+  galleryRef.insertAdjacentHTML('beforeend', arrayOfImg.join(''));
 }
 
 galleryRender(galleryItems);
-// 2. Реализация делегирования на div.gallery и получение url большого изображения.
-galleryRef.addEventListener('click', openOriginalImage)
+galleryRef.addEventListener('click', openOriginalImage);
 
 function openOriginalImage(event) {
     event.preventDefault();
 
     if (event.target.classList.contains('gallery__image')) {
-        const instance = basicLightbox.create(
-            `<img src="${event.target.dataset.source}" width="800" height="600">`
-        );
-        instance.show();
-
-        // 3. Закрытие с клавиатуры
-        document.addEventListener('keydown', onEscClick);
-        function onEscClick(event) {
-            if (event.code === 'Escape') {
-                instance.close()
-            }
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="800" height="600">`,
+        {
+        onShow: () => document.addEventListener('keydown', onEscClick),
+        onClose: () => document.removeEventListener('keydown', onEscClick),
         }
+    );
+    instance.show();
+
+    // 3. Закрытие с клавиатуры
+
+    function onEscClick(event) {
+        if (event.code === 'Escape') {
+        instance.close();
+        }
+    }
     }
 }
